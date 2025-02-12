abb3f7fa0433fc87a445b51cf3a2b69fcf99f29f
el132448 <134218938+el132448@users.noreply.github.com>
2025-02-12 02:39:51 +0000

    modified:   .devcontainer/setup-env.sh

--- .devcontainer/setup-env.sh
+++ .devcontainer/setup-env.sh
@@ -3,20 +3,21 @@
 # Set AWS environment variables
 # export AWS_ACCESS_KEY_ID=
 # export AWS_SECRET_ACCESS_KEY=
-# export AWS_DEFAULT_REGION=ap-northeast-1
+export AWS_DEFAULT_REGION=ap-northeast-1
 
 # Set AWS CLI auto prompt
-# export AWS_CLI_AUTO_PROMPT=on-partial
+export AWS_CLI_AUTO_PROMPT=on-partial
 
 # Install AWS CLI
-cd /workspace
+cd /workspaces
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -o awscliv2.zip
 sudo ./aws/install
 cd $CODESPACE_VSCODE_FOLDER
 
 # Verify installation
 # aws sts get-caller-identity
 # aws s3 ls
+# $ aws --version
 
 echo "Environment setup complete."
